bb245e812cc81af5288eaf38810039266eb431a4
panfilov.v <panfilov.v@bk.ru>
2025-04-12 18:56:13 +0300

    feat (clear queue) - added scheduler to clear old queue

---
+++ app/Console/Commands/ClearQueue.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Queue;
+use Illuminate\Console\Command;
+
+class ClearQueue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'workersQueue:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Очистить очереди ранее чем 7 дней';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Queue::query()->where('created_at', '<=', now()->subDays(7))->delete();
+    }
+}
