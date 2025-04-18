ac212c161a09280d2330a6b6da998a91b9f5b00a
panfilov.v <panfilov.v@bk.ru>
2025-04-19 00:10:41 +0300

    feat (tablesSeeder) - create tables for e7-e10

--- database/seeders/TablesSeeder.php
+++ database/seeders/TablesSeeder.php
@@ -10,15 +10,59 @@ use Illuminate\Support\Str;
 class TablesSeeder extends Seeder{
     public function run()
     {
-        for ($i = 1; $i <= 100; $i++){
+
+        // 4 - e9 96
+        // 5 - e10 185
+        // 6 - e7 180
+        // 7 - e8 64
+
+
+        for ($i = 1; $i <= 96; $i++){
             DB::table('tables')->insertOrIgnore([
                 'name' => ''. $i . ' стол',
                 'code' => ''.$i . ' стол',
-                'department_id' => 3,
+                'department_id' => 4,
                 'worker_id' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
         }
+
+        for ($i = 1; $i <= 185; $i++){
+            DB::table('tables')->insertOrIgnore([
+                'name' => ''. $i . ' стол',
+                'code' => ''.$i . ' стол',
+                'department_id' => 5,
+                'worker_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        for ($i = 1; $i <= 64; $i++){
+            DB::table('tables')->insertOrIgnore([
+                'name' => ''. $i . ' стол',
+                'code' => ''.$i . ' стол',
+                'department_id' => 7,
+                'worker_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        for ($i = 1; $i <= 180; $i++){
+            DB::table('tables')->insertOrIgnore([
+                'name' => ''. $i . ' стол',
+                'code' => ''.$i . ' стол',
+                'department_id' => 6,
+                'worker_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+
+
+
     }
 }
