a857594a985476bc47530e7b7ab742c41a864e2b
panfilov.v <panfilov.v@bk.ru>
2025-08-14 16:03:37 +0300

    feat (Incidents) - убрал очистку

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('workersQueue:clear')->daily();
 Schedule::command('reportEntities:clear')->daily();
-Schedule::command('incidents:clear')->everyFiveMinutes();
+//Schedule::command('incidents:clear')->everyFiveMinutes();
 Schedule::command('tables:clear')->dailyAt('08:00')->timezone('Europe/Moscow');
 Schedule::command('tables:clear')->dailyAt('20:00')->timezone('Europe/Moscow');
